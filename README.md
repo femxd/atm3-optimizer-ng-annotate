2b46789b2946db30bc2f559ba5eda7b8ddfabfe2
googleyufei <googleyufei@qq.com>
2015-09-28 16:34:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# qycloud-optimizer-ng-annotate
+
+压缩AngularJS代码前预处理。
+
+## 用法
+
+修改 fis-conf.js
+
+    fis.config.merge({
+        modules : {
+            optimizer : {
+                js : ['ng-annotate', 'uglify-js']
+            }
+        }
+    });
---
+++ index.js
@@ -0,0 +1,25 @@
+var esprima = require('esprima'),
+    escodegen = require('escodegen'),
+    astral = require('astral')();
+
+require('astral-angular-annotate')(astral);
+
+module.exports = function(content, file, conf) {
+    if (file.isJsLike && file.isAngular) {
+        var ast = esprima.parse(content, {
+            tolerant: true
+        });
+
+        astral.run(ast);
+
+        return escodegen.generate(ast, {
+            format: {
+                indent: {
+                    style: '  '
+                }
+            }
+        });
+    } else {
+        return content;
+    }
+};
